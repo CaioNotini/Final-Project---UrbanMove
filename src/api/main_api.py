1ed0d48eb5590ea88b047f39faefc440b0144cbd
CaioNotini <caio.notini86@gmail.com>
2026-04-09 18:10:36 +0200

    Adding GET graph functions

--- src/api/main_api.py
+++ src/api/main_api.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from src.api.vehicles_api import router as vehicles_router
 from src.api.traffic_api import router as traffic_router
+from src.api.graph_api import router as graph_router
+
 
 app = FastAPI(title="UrbanMove API")
 
 app.include_router(vehicles_router)
 app.include_router(traffic_router)
+app.include_router(graph_router)
 
 
 @app.get("/health")
