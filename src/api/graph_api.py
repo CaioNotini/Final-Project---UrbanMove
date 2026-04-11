8dce3e0243c2b0d2a80b67c68f6246c5a5e39925
CaioNotini <caio.notini86@gmail.com>
2026-04-11 22:28:03 +0200

    Update graph

--- src/api/graph_api.py
+++ src/api/graph_api.py
@@ -1,16 +1,11 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from src.utils.logging_config import setup_logger
 
-#from src.api.auth_api import require_admin
 from src.db.grid_db import read_graph, load_graph, create_graph
 from src.simulator.grid import create_city_graph
 
-router = APIRouter(
-    prefix="/graph",
-    tags=["graph"],
-)
-
+router = APIRouter(prefix="/graph", tags=["graph"])
 logger = setup_logger("graph-api", "api.log")
 
 
@@ -31,7 +26,7 @@ def get_or_create_graph(config: GraphConfig):
             logger.info("Loading graph from database")
             graph = load_graph()
         else:
-            logger.info("Creating new graph")
+            logger.info("Creating graph and saving to database")
             graph = create_city_graph(
                 config.width,
                 config.height,
@@ -40,12 +35,32 @@ def get_or_create_graph(config: GraphConfig):
             )
             create_graph(graph)
 
-        nodes = [{"id": n, "x": d["x"], "y": d["y"]} for n, d in graph.nodes(data=True)]
-        edges = [{"from_node": u, "to_node": v} for u, v in graph.edges()]
+        nodes = [
+            {"id": n, "x": data["x"], "y": data["y"]}
+            for n, data in graph.nodes(data=True)
+        ]
+
+        edges = [
+            {
+                "from_node": u,
+                "to_node": v,
+                "segment_id": data.get("segment_id"),
+                "weight": data.get("weight"),
+                "length_km": data.get("length_km"),
+                "speed_limit_kmh": data.get("speed_limit_kmh"),
+                "traffic_level": data.get("traffic_level"),
+                "traffic_multiplier": data.get("traffic_multiplier"),
+                "base_travel_time_h": data.get("base_travel_time_h"),
+            }
+            for u, v, data in graph.edges(data=True)
+        ]
 
         logger.info("Graph returned | nodes=%d | edges=%d", len(nodes), len(edges))
 
-        return {"nodes": nodes, "edges": edges}
+        return {
+            "nodes": nodes,
+            "edges": edges
+        }
 
     except Exception:
         logger.exception("POST /graph failed")
